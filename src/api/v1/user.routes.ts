af881c9ee2002f67b011d53cfd0e7c12ccc15d6a
Kevin Ly <kevinly92@hotmai.com>
2017-11-23 15:57:40 +0100

    Add Recipe model and routes

--- src/api/v1/user.routes.ts
+++ src/api/v1/user.routes.ts
@@ -1,5 +1,8 @@
 import express = require('express');
+
 import { User } from '../../model/user.model';
+
+import { Recipe } from '../../model/recipe.model';
 
 const routes = express.Router();
 
@@ -7,6 +10,46 @@ routes.get('/', (req, res, next) => {
     User.find({})
         .then((users) => {
             res.status(200).json(users);
+        })
+        .catch(next);
+});
+
+routes.get('/recipes', (req, res, next) => {
+    Recipe.find({})
+        .then((recipes) => {
+            res.status(200).json(recipes);
+        })
+        .catch(next);
+});
+
+routes.get('/recipes/:id', (req, res, next) => {
+    Recipe.find({id: req.query.id})
+        .then((recipe) => {
+            res.status(200).json(recipe);
+        })
+        .catch(next);
+});
+
+routes.post('/recipes', (req, res, next) => {
+    Recipe.create(req.body)
+        .then(() => {
+            res.status(201);
+        })
+        .catch(next);
+});
+
+routes.put('/recipes', (req, res, next) => {
+    Recipe.update({id: req.body.query.id}, req.body)
+        .then((recipe) => {
+            res.status(202).json(recipe);
+        })
+        .catch(next);
+});
+
+routes.delete('/recipes:id', (req, res, next) => {
+    Recipe.remove(req.query.id)
+        .then(() => {
+            res.status(204);
         })
         .catch(next);
 });
