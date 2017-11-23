4db5c27e2b9fe7091d887ae5eba8574e6c333943
Daniel van Bavel <dvanbavel@gmail.com>
2017-11-23 10:51:39 +0100

    changed env config settings

--- config/env/env.js
+++ config/env/env.js
@@ -1,10 +1,10 @@
 var env = {
     webPort: process.env.PORT || 3000,
-    dbHost: process.env.DB_HOST || 'localhost',
-    dbPort: process.env.DB_PORT || '',
-    dbUser: process.env.DB_USER || '',
-    dbPassword: process.env.DB_PASSWORD || '',
-    dbDatabase: process.env.DB_DATABASE || 'users'
+    dbHost: process.env.DB_HOST || 'ds117956.mlab.com',
+    dbPort: process.env.DB_PORT || '17956',
+    dbUser: process.env.DB_USER || 'admin',
+    dbPassword: process.env.DB_PASSWORD || 'admin123',
+    dbDatabase: process.env.DB_DATABASE || 'projectmongodb'
 }
 
 var dburl = process.env.NODE_ENV === 'production' ?
