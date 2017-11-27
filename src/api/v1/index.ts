17264917bc7e54898c0ca405226fe1f0046525a4
Daniel van Bavel <dvanbavel@gmail.com>
2017-11-27 13:32:42 +0100

    added routes

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,10 +1,12 @@
 import express = require('express');
 import recipeRoutes from './recipe.routes';
+import shoppinglistRoutes from './shoppinglist.routes';
 import userRoutes from './user.routes';
 
 const router = express.Router();
 
 router.use('/users', userRoutes);
 router.use('/recipes', recipeRoutes);
+router.use('/shoppinglist', shoppinglistRoutes);
 
 export default router;
