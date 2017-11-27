72535d0b14f246fc2987831f4c3c49ad5d6c5f73
Daniel van Bavel <dvanbavel@gmail.com>
2017-11-27 20:21:14 +0100

    schema changed

--- src/model/schemas/recipe.schema.ts
+++ src/model/schemas/recipe.schema.ts
@@ -3,12 +3,16 @@ import { IIngredientDocument, IngredientSchema } from './ingredient.schema';
 
 export interface IRecipeDocument extends Document {
     name: string;
+    description: string,
+    imagePath: string,
     time: string;
     ingredients: [IIngredientDocument];
 }
 
 export const RecipeSchema: Schema = new Schema({
     name: String,
+    description: String,
+    imagePath: String,
     time: String,
     ingredients: [IngredientSchema]
 });
